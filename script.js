bf881b1ccd4b2669d2e5f5a2c2b9026558ed16fb
vaidika1410 <vaidikakaul20@gmail.com>
2025-06-05 18:36:06 +0530

    Complete Featured Projects page and Create Milestones & Certifications page

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+function locomotive(){
+  gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+
+  // for tablet smooth
+  tablet: { smooth: true },
+
+  // for mobile
+  smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+
+locomotive()
+
+
+function navbarAnimation(){
+  gsap.to("nav", {
+    // duration: 1.5,
+    height: "8vh",
+    borderBottom: "1px solid rgba(255, 255, 255, 0.39)",
+    scrollTrigger: {
+      trigger: "nav",
+      scroller: ".main",
+      start: "top -10%",
+      scrub: 2,
+    }
+  })
+}
+navbarAnimation()
+
+
 function nameTyping(){
   new Typed('#typed-text', {
     strings: [
@@ -17,3 +73,83 @@ function nameTyping(){
 }
 
 nameTyping()
+
+function featuredProjects(){
+  let filterButtons = document.querySelectorAll(".filter-buttons a");
+  let projectCards = document.querySelectorAll(".project-card");
+
+console.log(filterButtons)
+console.log(projectCards)
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", ()=>{
+    const filter = button.getAttribute('data-filter');
+
+    projectCards.forEach((card) => {
+      const category = card.getAttribute('data-category')
+      if (filter === category) {
+        card.style.display = 'block';
+        gsap.from(card, {
+          duration: 0.2,
+          opacity: 0
+        })
+      } else {
+        card.style.display = 'none';
+      }
+    })
+  })
+})
+}
+
+featuredProjects();
+
+function featuredProjectsButton(){
+  let button = document.querySelector(".filter-buttons .button")
+
+button.addEventListener('mouseenter', function(){
+  gsap.to(button, {
+    scale: 1.1,
+  })
+
+  gsap.to(".button-text-upper p", {
+  y: -10,
+  opacity: 0,
+  duration: 0.1,
+  stagger: { amount: 0.4 },
+  ease: "power2.out"
+});
+gsap.to(".button-text-lower p", {
+  y: -18,
+  opacity: 1,
+  duration: 0.1,
+  stagger: { amount: 0.4 },
+  ease: "power2.out"
+});
+
+})
+
+button.addEventListener('mouseleave', function(){
+  gsap.to(button, {
+    scale: 1,
+  })
+
+  gsap.to(".button-text-upper p", {
+  y: 0,
+  opacity: 1,
+  duration: 0.1,
+  stagger: { amount: 0.4 },
+  ease: "power2.out"
+});
+gsap.to(".button-text-lower p", {
+  y: 0,
+  opacity: 0,
+  duration: 0.1,
+  stagger: { amount: 0.4 },
+  ease: "power2.out"
+});
+
+})
+
+}
+
+featuredProjectsButton();
